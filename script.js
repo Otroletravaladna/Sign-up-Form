a1d8e0d857b864b75ec616e6d464af29ea4e0d69
Otroletravaladna <crnana98@gmail.com>
2022-11-17 02:10:23 -0300

    Edit password check and valid/invalid style

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const invalidColor = document.querySelector(":root");
 const input = document.querySelectorAll("input");
+const confirmPass = document.getElementById("pass-confirm");
+const pass = document.getElementById("pass");
+const submit = document.querySelector("button");
+
+
 
 
 input.forEach((item) => {
@@ -7,4 +12,13 @@ input.forEach((item) => {
         invalidColor.style.setProperty("--required", "red");
         item.classList.add("input");
     })
+})
+
+submit.addEventListener("click", () => {
+    if (confirmPass.value === pass.value) {
+        confirmPass.setCustomValidity("");
+    } else if (confirmPass.value != pass.value) {
+        confirmPass.setCustomValidity("Passwords must match");
+        confirmPass.reportValidity();
+    }
 })
